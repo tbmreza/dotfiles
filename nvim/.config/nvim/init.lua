53dba19270889c9fd31df4c1a543f68124bd2dbf
Reza Handzalah <rezahandzalah@gmail.com>
2022-04-24 12:53:36 +0700

    first pass done
    mostly untested. expect bug fixes.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("plugins")
+require("tbmreza.rust")
 
 local o = vim.opt
 local wo = vim.wo
@@ -17,6 +18,9 @@ o.smartcase = true
 bo.formatoptions = "jcql"
 o.list = true
 o.listchars = "eol:¬,tab:▸\\"
+o.termguicolors = true
+o.sidescroll = 1
+o.sidescrolloff = 10
 
 local def = vim.api.nvim_create_user_command
 def("Vimrc", "tabe $HOME/.dotfiles/nvim/.config/nvim/init.lua", { nargs = 0 })
@@ -24,11 +28,53 @@ def("Up", "cd ..", { nargs = 0 })
 def("Back", "cd -", { nargs = 0 })
 def("Scroll", "windo set scrollbind", { nargs = 0 })
 def("ScrollOff", "windo set scrollbind!", { nargs = 0 })
+def("Pwd", 'let @+ = expand("%")', { nargs = 0 })
 
 -- remaps in separate file?
 vim.g.mapleader = " "
-
 local map = vim.api.nvim_set_keymap
+
+map("t", "<f4>", "<c-\\><c-n>", { noremap = true })
+
+-- Merge conflicts
+map("n", "<leader>gj", ":diffget //3<cr>", { noremap = false })
+map("n", "<leader>gf", ":diffget //2<cr>", { noremap = false })
+
+-- Abort sandwich
+map("n", "s<esc>", "<nop>", { noremap = true, silent = true })
+
+-- Must have typed this by mistake
+map("n", "ZZ", "<nop>", { noremap = true })
+map("n", "-", "<nop>", { noremap = true })
+
+-- Move lines
+map("n", "<c-j>", ":m .+1<CR>==", { noremap = true })
+map("n", "<c-k>", ":m .-2<CR>==", { noremap = true })
+map("v", "<c-j>", ":m '>+1<CR>gv=gv", { noremap = true })
+map("v", "<c-k>", ":m '<-2<CR>gv=gv", { noremap = true })
+
+-- IDE
+-- map("n", "<leader>x", ":sp<cr>:SQHExecuteFile<cr>", { noremap = true })
+map("n", "<leader>b", ":Vexplore<cr>", { noremap = true })
+map("n", "<leader>t", ":Telescope<space>", { noremap = true })
+map("n", "<c-p>", ":Files<cr>", { noremap = true, silent = true })
+map("n", "<c-f>", ":Rg<cr>", { noremap = true, silent = true })
+map("n", "<c-h>", ":History<cr>", { noremap = true, silent = true })
+-- Terminal command integration
+map("n", "<leader>g", ":Git<space>", { noremap = true })
+map("n", "<leader>c", ":Cargo<space>", { noremap = true })
+
+-- Cycle through tabs
+map("", "<tab>", "gt", { noremap = true })
+map("", "<s-tab>", "gT", { noremap = true })
+map("n", "<c-d>", "<c-y>", { noremap = true })
+map("n", "<c-y>", "<s-tab>", { noremap = false })
+map("n", "[b", ":bprev<cr>", { noremap = false })
+map("n", "]b", ":bnext<cr>", { noremap = false })
+
+-- Horizontal scrolling
+map("", "<C-ScrollWheelDown>", "10zl", { noremap = true, silent = true })
+map("", "<C-ScrollWheelUp>", "10zh", { noremap = true, silent = true })
 
 -- Plugin: mundo
 vim.g.mundo_help = 1
@@ -108,7 +154,57 @@ vim.g.lightline = {
 	},
 }
 
+vim.cmd([[
+	syntax enable
+	filetype plugin indent on
+
+	function! FiletypeCoq()
+		nmap <leader>j :CoqNext<cr>
+		nmap <leader>k :CoqUndo<cr>
+		nmap <leader>l :CoqToLine<cr>
+	endfunction
+
+	function! FiletypePHP()
+		vnoremap <leader>dd yodd(<c-r>0);<esc>
+		nnoremap <leader>dd yeodd(<c-r>0);<esc>
+	endfunction
+
+	autocmd filetype coq call FiletypeCoq()
+	autocmd filetype php call FiletypePHP()
+	autocmd BufRead,BufNewFile *.volt setfiletype html
+	autocmd User CocStatusChange,CocDiagnosticChange call lightline#update()
+
+	silent! call repeat#set("zfi{")
+	silent! call repeat#set("zfib")
+	silent! call repeat#set("zfip")
+]])
+
+-- Plugin: indentLine
+vim.g.indentLine_char = "·"
+
+-- Plugin: Coqtail
+vim.g.coqtail_noimap = 1
+
+-- Plugin: rust.vim
+vim.g.rustfmt_autosave = 0
+
+-- Plugin: coc
+map("n", "gd", "<Plug>(coc-definition)", { noremap = false })
+map("n", "gy", "<Plug>(coc-type-definition)", { noremap = false })
+map("n", "gi", "<Plug>(coc-implementation)", { noremap = false })
+map("n", "gr", "<Plug>(coc-references)", { noremap = false })
+map("n", "<f2>", "<Plug>(coc-rename)", { noremap = false })
+
+-- Plugin: blamer
+vim.g.blamer_enabled = 1
+vim.g.blamer_show_in_visual_modes = 0
+vim.g.blamer_show_in_insert_modes = 0
+
+-- Plugin: rainbow
+vim.g.rainbow_active = 0
+
 -- Plugin: neoformat
+vim.g.neoformat_try_formatprg = 1
 map("n", "<leader>f", ":Neoformat<cr>", { noremap = false })
 map("v", "<leader>f", ":Neoformat!<space>", { noremap = false })
 
@@ -129,6 +225,16 @@ vim.g.neoformat_racket_fmt = {
 vim.g.neoformat_enabled_javascript = { "prettier" }
 vim.g.neoformat_enabled_json5 = { "prettier" }
 vim.g.neoformat_enabled_racket = { "fmt" }
+
+-- Plugin: SQHell
+vim.g.sqh_provider = "sqlite"
+local default_db = {
+	database = "/Users/reza.handzalah/work/cmu-db-course/homework_sql/musicbrainz-cmudb2020.db",
+}
+vim.g.sqh_connections = {
+	-- conn_name = db,
+	default = default_db,
+}
 
 -- Closing pair
 map("i", '"', '""<left>', { noremap = true })
@@ -171,184 +277,23 @@ map("n", "<leader>>", "a=>", { noremap = true })
 map("n", "<leader>:", "a->", { noremap = true })
 map("n", "<esc><esc>", ":nohlsearch<cr>", { noremap = true, silent = true })
 
+-- netrw
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 2
+vim.g.netrw_list_hide = ".*.swp$,.DS_Store,*/tmp/*,*.so,*.swp,*.zip,*.git,^..=/=$"
+vim.g.netrw_keepdir = 0
+
+-- svelte
+vim.g.vim_svelte_plugin_load_full_syntax = 1
+vim.g.vim_svelte_plugin_use_typescript = 1
+
 -- Theme
+vim.g.colorscheme_switcher_exclude_builtins = 1
 vim.cmd([[
-  function! SetColorscheme()
-    colorscheme melange
-    hi! Normal guibg=NONE ctermbg=NONE " transparent window if terminal supports it
-  endfunction
-  autocmd vimenter * ++nested call SetColorscheme()
-]])
-
--- TODO
-vim.cmd([[
-" Merge conflicts
-nmap <leader>gj :diffget //3<cr>
-nmap <leader>gf :diffget //2<cr>
-" Abort sandwich
-nnoremap <silent> s<esc> <nop>
-" Must have typed this by mistake
-nnoremap ZZ <nop>
-nnoremap - <nop>
-" Move lines
-nnoremap <c-j> :m .+1<CR>==
-nnoremap <c-k> :m .-2<CR>==
-vnoremap <c-j> :m '>+1<CR>gv=gv
-vnoremap <c-k> :m '<-2<CR>gv=gv
-" }}
-" IDE {{
-" inoremap <c-j> <Plug>JiraComplete  " needs python provider
-nnoremap <leader>x :sp<cr>:SQHExecuteFile<cr>
-nnoremap <leader>b :Vexplore<cr>
-" nnoremap <silent> <c-p> :Files<cr>
-" nnoremap <silent> <c-f> :Rg<cr>
-nnoremap <leader>t :Telescope<space>
-" nnoremap <silent> <c-p> :Telescope find_files<cr>
-nnoremap <silent> <c-p> :Files<cr>
-" nnoremap <silent> <c-f> :Telescope live_grep<cr>
-nnoremap <silent> <c-f> :Rg<cr>
-nnoremap <silent> <c-h> :History<cr>
-" }}
-" Terminal command integration {{
-nnoremap <leader>g :Git<space>
-nnoremap <leader>c :Cargo<space>
-" }}
-" Cycle through tabs {{
-noremap <tab> gt
-noremap <s-tab> gT
-nnoremap <c-d> <c-y>
-nmap <c-y> <s-tab>
-nmap [b :bprev<cr>
-nmap ]b :bnext<cr>
-" }}
-" Horizontal scrolling (ugly but here we are) {{
-set sidescroll=1
-set sidescrolloff=10
-noremap <silent><C-ScrollWheelDown>   10zl
-noremap <silent><C-2-ScrollWheelDown> 10zl
-noremap <silent><C-3-ScrollWheelDown> 10zl
-noremap <silent><C-4-ScrollWheelDown> 10zl
-noremap <silent><C-ScrollWheelUp>     10zh
-noremap <silent><C-2-ScrollWheelUp>   10zh
-noremap <silent><C-3-ScrollWheelUp>   10zh
-noremap <silent><C-4-ScrollWheelUp>   10zh
-" }}
-" Copy buffer file path {{
-noremap <f1> :let @+ = expand("%")<cr>
-" Exit terminal mode
-tnoremap <f4> <c-\><c-n>
-" }}
-" netrw {{
-let g:netrw_liststyle = 3
-let g:netrw_banner = 0
-let g:netrw_browse_split = 2
-let g:netrw_list_hide= '.*\.swp$,.DS_Store,*/tmp/*,*.so,*.swp,*.zip,*.git,^\.\.\=/\=$'
-let g:netrw_keepdir=0
-" let g:netrw_winsize = 20
-" }}
-
-" leafOfTree/vim-svelte-plugin
-let g:vim_svelte_plugin_load_full_syntax = 1
-let g:vim_svelte_plugin_use_typescript = 1
-
-" Theme {{
-set termguicolors
-let g:colorscheme_switcher_exclude_builtins = 1
-
-" Filetype by filetype {{
-filetype plugin on
-autocmd BufRead,BufNewFile *.volt setfiletype html
-" autocmd BufRead,BufNewFile *.abs setfiletype abs
-" syntax/synload.vim doesn't load?
-" au Syntax abs    runtime! syntax/abs.vim
-
-"let g:neoformat_javascript_prettier = {
-"           \ 'exe': 'prettier',
-"           \ 'args': ['--write'],
-"           \ 'replace': 1,
-"           \ }
-
-"let g:neoformat_json5_prettier = {
-"           \ 'exe': 'prettier',
-"           \ 'args': ['--write'],
-"           \ 'replace': 1,
-"           \ }
-
-"let g:neoformat_racket_fmt = {
-"           \ 'exe': 'raco',
-"           \ 'args': ['fmt', '-i'],
-"           \ 'replace': 1,
-"           \ }
-
-"let g:neoformat_enabled_javascript = ['prettier']
-"let g:neoformat_enabled_json5 = ['prettier']
-"let g:neoformat_enabled_racket = ['fmt']
-
-"nmap <leader>f :Neoformat<cr>
-"vmap <leader>f :Neoformat!<space>
-
-function! FiletypeCoq()
-  nmap <leader>j :CoqNext<cr>
-  nmap <leader>k :CoqUndo<cr>
-  nmap <leader>l :CoqToLine<cr>
-  " silent! call repeat#set("<leader>j")
-  " silent! call repeat#set("<leader>k")
-  " silent! call repeat#set("<leader>l")
-endfunction
-
-function! FiletypePHP()
-  vnoremap <leader>dd yodd(<c-r>0);<esc>
-  nnoremap <leader>dd yeodd(<c-r>0);<esc>
-endfunction
-
-autocmd filetype coq call FiletypeCoq()
-" autocmd filetype rust call FiletypeRust()
-lua require('tbmreza.rust')
-" autocmd filetype svelte,javascript,javascriptreact,typescript,typescriptreact,json,graphql,css,markdown call FiletypePrettier()
-autocmd filetype php call FiletypePHP()
-" }}
-
-autocmd User CocStatusChange,CocDiagnosticChange call lightline#update()
-
-" Plugin configs {{
-" indentLine
-let g:indentLine_char = '·'
-" Coqtail
-let g:coqtail_noimap = 1
-" let g:coqtail_map_prefix = '<leader>q'
-" rust.vim
-syntax enable
-filetype plugin indent on
-let g:rustfmt_autosave = 0
-" coc.nvim
-nmap gd <Plug>(coc-definition)
-nmap gy <Plug>(coc-type-definition)
-nmap gi <Plug>(coc-implementation)
-nmap gr <Plug>(coc-references)
-nmap <f2> <Plug>(coc-rename)
-" blamer
-let g:blamer_enabled = 1
-let g:blamer_show_in_visual_modes = 0
-let g:blamer_show_in_insert_modes = 0
-" rainbow
-let g:rainbow_active = 0
-" syntastic
-let g:syntastic_always_populate_loc_list = 1
-let g:syntastic_auto_loc_list = 1
-" neoformat
-let g:neoformat_try_formatprg = 1
-" SQHell
-let g:sqh_provider = 'sqlite'
-let g:sqh_connections = {
-      \ 'course': {
-        \ 'database': '/Users/reza.handzalah/work/cmu-db-course/homework_sql/musicbrainz-cmudb2020.db'
-        \ },
-        \ 'default': {
-          \ 'database': '/Users/reza.handzalah/work/cmu-db-course/homework_sql/musicbrainz-cmudb2020.db'
-          \ }
-          \ }
-
-silent! call repeat#set("zfi{")
-silent! call repeat#set("zfib")
-silent! call repeat#set("zfip")
+	function! SetColorscheme()
+		colorscheme melange
+		hi! Normal guibg=NONE ctermbg=NONE " transparent window if terminal supports it
+	endfunction
+	autocmd vimenter * ++nested call SetColorscheme()
 ]])
